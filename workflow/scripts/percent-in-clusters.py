a715c7cc1c91780a952c8bd12eb033b2eda87a7b
Mitchell Robert Vollger <mrvollger@gmail.com>
2023-06-01 12:50:41 -0700

    black formating

---
+++ workflow/scripts/percent-in-clusters.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import sys
+
+def my_groupby(gdf):
+    real=gdf[gdf[5] == "Real"]
+    null=gdf[gdf[5] == "Null"]
+    return real.length.sum() - null.length.sum()
+
+bed=sys.argv[1]
+df = pd.read_csv(bed, sep="\t", header=None)
+df["length"] = df[2] - df[1]
+df = df.groupby([4]).apply(my_groupby).reset_index()
+print(df)
