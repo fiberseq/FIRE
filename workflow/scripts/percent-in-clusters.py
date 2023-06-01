b7ea1db4f9703aa6faccd81214ee71f56d76753d
Mitchell Robert Vollger <mrvollger@gmail.com>
2023-06-01 14:36:33 -0700

    black formating

--- workflow/scripts/percent-in-clusters.py
+++ workflow/scripts/percent-in-clusters.py
@@ -4,13 +4,13 @@ import sys
 def my_groupby(gdf):
     real=gdf[gdf.case == "Real"]
     null=gdf[gdf.case == "Null"]
-    return real.length.sum() - null.length.sum()
+    return (real.length*real.cov).sum() - (null.length*real.cov).sum()
 
 bed=sys.argv[1]
 df = pd.read_csv(bed, sep="\t", header=None)
 df.columns = ["ct", "st", "en", "cov", "case"]
 df["length"] = df.en - df.st
-real_bp = df[df.case == "Real"].length.sum()
+real_bp = (df[df.case == "Real"].length * df[df.case == "Real"].cov).sum()
 print(df)
 df = df.groupby("cov").apply(my_groupby).reset_index()
 print(df)
