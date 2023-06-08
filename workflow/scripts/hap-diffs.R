21bf347e3e5b84c9d7b55b2c94eae588bb1c6683
Mitchell Robert Vollger <mrvollger@gmail.com>
2023-06-08 08:41:20 -0700

    black formating

--- workflow/scripts/hap-diffs.R
+++ workflow/scripts/hap-diffs.R
@@ -97,7 +97,9 @@ out_file_3=snakemake@output[[1]]
 df=fread(in_file)
 print(sapply(df, class))
 cols.num <- c("hap1_acc","hap2_acc","hap1_link","hap2_link","hap1_nuc","hap2_nuc")
-df[cols.num] <- sapply(df[cols.num], as.numeric)
+for (col in cols.num){
+    df[col] <- as.numeric(df[col])
+}
 print(sapply(df, class))
 
 # continue 
