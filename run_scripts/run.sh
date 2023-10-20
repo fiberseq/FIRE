fb7a26e62b277d50e121784c9a211730c67b6675
Mitchell R. Vollger <mrvollger@gmail.com>
2023-10-19 21:25:08 -0700

    chore: update.

--- run_scripts/run.sh
+++
@@ -1,8 +0,0 @@
-
-snakemake --configfile config/config_t2t_v2.0.yaml --cores $(nproc) --rerun-triggers mtime \
-  $@
-
-exit
-
-snakemake --configfile config/config.yaml --cores $(nproc) --rerun-triggers mtime \
-  $@
