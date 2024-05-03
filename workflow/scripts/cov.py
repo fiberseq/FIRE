160821ff2751d41d5e915224f2a0906e709b849b
Mitchell Robert Vollger <mrvollger@gmail.com>
2024-05-03 09:51:48 -0700

    use python script

--- workflow/scripts/cov.py
+++ workflow/scripts/cov.py
@@ -22,6 +22,7 @@ def weighted_median(df, val, weight):
     df.sort_values(val, inplace=True)
     df["cumsum"] = df[weight].cumsum()
     df["cutoff"] = df[weight].sum() / 2.0
+    print(df, file=sys.stderr)
     comparison = df[df["cumsum"] >= df["cutoff"]][val]
     print(comparison, file=sys.stderr)
     return comparison.iloc[0]
