49a81725b363ac5657232b1c24c1a1919307ddc8
Mitchell R. Vollger <mvollger@gmai.com>
2025-11-04 10:14:28 -0500

    plot legends

--- workflow/scripts/hap-diffs.R
+++ workflow/scripts/hap-diffs.R
@@ -4,23 +4,23 @@ library(scales)
 library(ggforce)
 library(cowplot)
 library(dplyr)
-#library(splitstackshape)
-#library(ggridges)
-#library(IRanges)
+# library(splitstackshape)
+# library(ggridges)
+# library(IRanges)
 library(ggrepel)
-#library(ggnewscale)
-#library(ggside)
+# library(ggnewscale)
+# library(ggside)
 library(glue)
-#library("tidylog", warn.conflicts = FALSE)
-#library(patchwork)
-#library(ggh4x)
+# library("tidylog", warn.conflicts = FALSE)
+# library(patchwork)
+# library(ggh4x)
 library(tools)
-#library(purrr)
-#library(reticulate)
+# library(purrr)
+# library(reticulate)
 library(ggpubr)
-#library(weights)
-#library(karyoploteR)
-#library(zoo)
+# library(weights)
+# library(karyoploteR)
+# library(zoo)
 library(scales)
 library(ggplot2)
 library(ggforce)
@@ -28,61 +28,63 @@ library(grid)
 options(scipen = 999)
 
 
-Red="#c1272d"
-Indigo="#0000a7"
-Yellow="#eecc16"
-Teal="#008176"
-Gray="#b3b3b3"
-FONT_SIZE=8
-MODEL_COLORS = c(PacBio=Indigo, 
-    CNN=Red,  
-    XGB=Yellow,
-    GMM=Teal,
-    IPD=Gray,
-    SEMI="purple",
-    Revio="#f41c90" # black
+Red <- "#c1272d"
+Indigo <- "#0000a7"
+Yellow <- "#eecc16"
+Teal <- "#008176"
+Gray <- "#b3b3b3"
+FONT_SIZE <- 8
+MODEL_COLORS <- c(
+    PacBio = Indigo,
+    CNN = Red,
+    XGB = Yellow,
+    GMM = Teal,
+    IPD = Gray,
+    SEMI = "purple",
+    Revio = "#f41c90" # black
 )
 
-my_ggsave <- function(file, ...){
-    file = glue(file)
+my_ggsave <- function(file, ...) {
+    file <- glue(file)
     print(file)
-    ext = file_ext(file)
-    file_without_ext = tools::file_path_sans_ext(file)
-    ggsave(glue("tmp.{ext}"), bg='transparent', ...)
-    cmd = glue("cp tmp.{ext} {file}")
-    fwrite(last_plot()$data, file=file_without_ext + ".tbl.gz", sep="\t")
+    ext <- file_ext(file)
+    file_without_ext <- tools::file_path_sans_ext(file)
+    ggsave(glue("tmp.{ext}"), bg = "transparent", ...)
+    cmd <- glue("cp tmp.{ext} {file}")
+    fwrite(last_plot()$data, file = file_without_ext + ".tbl.gz", sep = "\t")
     print(cmd)
     system(cmd)
 }
 
-my_grid = function(...){
-    theme_minimal_grid(font_size=FONT_SIZE, ...)
-} 
+my_grid <- function(...) {
+    theme_minimal_grid(font_size = FONT_SIZE, ...)
+}
 
-my_hgrid = function(...){
-    theme_minimal_hgrid(font_size=FONT_SIZE, ...)
-} 
+my_hgrid <- function(...) {
+    theme_minimal_hgrid(font_size = FONT_SIZE, ...)
+}
 
-my_vgrid = function(...){
-    theme_minimal_vgrid(font_size=FONT_SIZE, ...)
-} 
+my_vgrid <- function(...) {
+    theme_minimal_vgrid(font_size = FONT_SIZE, ...)
+}
 
 reverselog_trans <- function(base = exp(1)) {
     trans <- function(x) -log(x, base)
     inv <- function(x) base^(-x)
-    trans_new(paste0("reverselog-", format(base)), trans, inv, 
-              log_breaks(base = base), 
-              domain = c(1e-100, Inf))
+    trans_new(paste0("reverselog-", format(base)), trans, inv,
+        log_breaks(base = base),
+        domain = c(1e-100, Inf)
+    )
 }
 
 scientific_10 <- function(x) {
-    is_one = as.numeric(x) == 1
-    text = gsub("e", " %*% 10^", scales::scientific_format()(x))
+    is_one <- as.numeric(x) == 1
+    text <- gsub("e", " %*% 10^", scales::scientific_format()(x))
     print(text)
-    text = str_remove(text, "^1 %\\*% ") # remove leading one 
+    text <- str_remove(text, "^1 %\\*% ") # remove leading one
     print(text)
-    text[is_one] = "10^0"
-    rtn = parse(text=text)
+    text[is_one] <- "10^0"
+    rtn <- parse(text = text)
     rtn
 }
 
@@ -91,17 +93,17 @@ scientific_10 <- function(x) {
 # SCRIPT
 #
 #
-p_threshold=0.05
-in_file=snakemake@input[[1]]
-out_file_1=snakemake@output[[1]]
-out_file_2=snakemake@output[[2]]
-out_file_3=snakemake@output[[3]]
-out_file_4=snakemake@output[[4]]
+p_threshold <- 0.05
+in_file <- snakemake@input[[1]]
+out_file_1 <- snakemake@output[[1]]
+out_file_2 <- snakemake@output[[2]]
+out_file_3 <- snakemake@output[[3]]
+out_file_4 <- snakemake@output[[4]]
 
 
-df=fread(in_file) %>%
+df <- fread(in_file) %>%
     mutate_at(
-        c("fire_coverage_H1","fire_coverage_H2","coverage_H1","coverage_H2"),
+        c("fire_coverage_H1", "fire_coverage_H2", "coverage_H1", "coverage_H2"),
         as.numeric
     ) %>%
     filter(pass_coverage) %>%
@@ -112,8 +114,8 @@ df=fread(in_file) %>%
         !is.na(coverage_H2),
     ) %>%
     mutate(
-        st=peak_start,
-        en=peak_end,
+        st = peak_start,
+        en = peak_end,
         `#ct` = `#chrom`,
     ) %>%
     data.table()
@@ -121,30 +123,30 @@ df=fread(in_file) %>%
 print("Before coverage filter")
 print(nrow(df))
 
-# continue 
-df$hap1_frac_acc = df$fire_coverage_H1/df$coverage_H1
-df$hap2_frac_acc = df$fire_coverage_H2/df$coverage_H2
-df$autosome = "Autosome"
-df[`#chrom` == "chrY"]$autosome = "chrY"
-df[`#chrom` == "chrX"]$autosome = "chrX"
+# continue
+df$hap1_frac_acc <- df$fire_coverage_H1 / df$coverage_H1
+df$hap2_frac_acc <- df$fire_coverage_H2 / df$coverage_H2
+df$autosome <- "Autosome"
+df[`#chrom` == "chrY"]$autosome <- "chrY"
+df[`#chrom` == "chrX"]$autosome <- "chrX"
 
 # filter by coverage
-sd = 3
-cov = median(df$coverage)
-my_min_cov = max(cov*0.5 - sd * sqrt(cov*0.5), 10)
-my_max_cov = cov*0.5 + sd * sqrt(cov*0.5)
-#my_min_cov = 10
-#my_max_cov = median(df$coverage)
+sd <- 3
+cov <- median(df$coverage)
+my_min_cov <- max(cov * 0.5 - sd * sqrt(cov * 0.5), 10)
+my_max_cov <- cov * 0.5 + sd * sqrt(cov * 0.5)
+# my_min_cov = 10
+# my_max_cov = median(df$coverage)
 
-#print(glue("min_cov={my_min_cov} max_cov={my_max_cov} cov={cov}"))
+# print(glue("min_cov={my_min_cov} max_cov={my_max_cov} cov={cov}"))
 
-pdf = df %>%
+pdf <- df %>%
     filter(coverage_H1 > 0 & coverage_H2 > 0) %>%
     mutate(
         min_cov = my_min_cov,
         max_cov = my_max_cov,
     ) %>%
-    filter(autosome != "chrY" ) %>%
+    filter(autosome != "chrY") %>%
     filter(coverage_H1 > min_cov & coverage_H2 > min_cov) %>%
     filter(coverage_H1 < max_cov & coverage_H2 < max_cov) %>%
     mutate(
@@ -157,7 +159,7 @@ pdf = df %>%
 print("After coverage filter")
 print(nrow(pdf))
 
-if(nrow(pdf)== 0){
+if (nrow(pdf) == 0) {
     system(glue("touch {out_file_1}"))
     system(glue("touch {out_file_2}"))
     system(glue("touch {out_file_3}"))
@@ -165,21 +167,21 @@ if(nrow(pdf)== 0){
     quit()
 }
 
-pdf = pdf %>%
+pdf <- pdf %>%
     filter(
-      hap1_acc >= 0,
-      hap1_nacc >= 0,
-      hap2_acc >= 0,
-      hap2_nacc >= 0,
+        hap1_acc >= 0,
+        hap1_nacc >= 0,
+        hap2_acc >= 0,
+        hap2_nacc >= 0,
     ) %>%
     rowwise() %>%
     mutate(
-        p_value=fisher.test(matrix(c(hap1_acc, hap1_nacc, hap2_acc, hap2_nacc),nrow=2))$p.value
+        p_value = fisher.test(matrix(c(hap1_acc, hap1_nacc, hap2_acc, hap2_nacc), nrow = 2))$p.value
     ) %>%
     ungroup() %>%
     group_by(autosome) %>%
     mutate(
-        p_adjust = p.adjust(p_value, method="BH"),
+        p_adjust = p.adjust(p_value, method = "BH"),
     ) %>%
     select(!starts_with("V")) %>%
     mutate(
@@ -194,66 +196,67 @@ print("After p-value adjustment")
 print(nrow(pdf %>% filter(p_adjust <= p_threshold)))
 
 # make the plots
-tdf = pdf 
+tdf <- pdf
 
 tdf %>%
-    ggplot(aes(x=hap1_frac_acc, y=hap2_frac_acc)) +
-    stat_cor(size=2) +
-    geom_hex(bins=75) +
-    geom_abline(aes(intercept=0, slope=1), linetype="dashed")+
-    scale_fill_distiller("", palette = "Spectral", trans="log10") +
-    scale_x_continuous("Paternal accessibility", labels=percent) +
-    scale_y_continuous("Maternal accessibility", labels=percent) +
-    #annotation_logticks(sides="lb") +
-    facet_wrap(~autosome, ncol=2)+
+    ggplot(aes(x = hap1_frac_acc, y = hap2_frac_acc)) +
+    stat_cor(size = 2) +
+    geom_hex(bins = 75) +
+    geom_abline(aes(intercept = 0, slope = 1), linetype = "dashed") +
+    scale_fill_distiller("", palette = "Spectral", trans = "log10") +
+    scale_x_continuous("H1 accessibility", labels = percent) +
+    scale_y_continuous("H2 accessibility", labels = percent) +
+    # annotation_logticks(sides="lb") +
+    facet_wrap(~autosome, ncol = 2) +
     my_grid()
-my_ggsave(out_file_1, height=3, width=6)
+my_ggsave(out_file_1, height = 3, width = 6)
 
-cor_p_threshold = max(tdf[p_adjust <= p_threshold & !is.na(p_value) & !is.na(p_adjust) ]$p_value)
+cor_p_threshold <- max(tdf[p_adjust <= p_threshold & !is.na(p_value) & !is.na(p_adjust)]$p_value)
 print(cor_p_threshold)
-y_lim = ceiling(max(max(-log10(tdf$p_value)), max(-log10(tdf$p_adjust)))) 
-y_by = 1 
-if(y_lim > 10){
-    y_by = 2
+y_lim <- ceiling(max(max(-log10(tdf$p_value)), max(-log10(tdf$p_adjust))))
+y_by <- 1
+if (y_lim > 10) {
+    y_by <- 2
 }
 # add p-value col, volcano plot
-n=comma(nrow(tdf))
-p = tdf %>%
-    ggplot(aes(x=diff, y=p_value)) +
-    geom_hex(bins=100) + scale_fill_distiller("", palette = "Spectral", trans="log10") +
-    geom_hline(aes(yintercept=(p_threshold)), linetype="dashed", color="darkblue")+
-    geom_hline(aes(yintercept=(cor_p_threshold)), linetype="dashed", color="darkred")+
-    facet_wrap(~autosome, ncol=2)+
-    scale_x_continuous("Difference between paternal and maternal accessibility", labels=percent) +
+n <- comma(nrow(tdf))
+p <- tdf %>%
+    ggplot(aes(x = diff, y = p_value)) +
+    geom_hex(bins = 100) +
+    scale_fill_distiller("", palette = "Spectral", trans = "log10") +
+    geom_hline(aes(yintercept = (p_threshold)), linetype = "dashed", color = "darkblue") +
+    geom_hline(aes(yintercept = (cor_p_threshold)), linetype = "dashed", color = "darkred") +
+    facet_wrap(~autosome, ncol = 2) +
+    scale_x_continuous("Difference between H1 and H2 accessibility", labels = percent) +
     scale_y_continuous(
-        glue("p-value   (n = {n})"), 
-        #labels=comma,
-        breaks=10**(-seq(0, y_lim, y_by)),
-        minor_breaks=10**(-seq(0, y_lim, 0.1)),
-        trans=reverselog_trans(10),
-        labels=scientific_10,
-    ) + 
+        glue("p-value   (n = {n})"),
+        # labels=comma,
+        breaks = 10**(-seq(0, y_lim, y_by)),
+        minor_breaks = 10**(-seq(0, y_lim, 0.1)),
+        trans = reverselog_trans(10),
+        labels = scientific_10,
+    ) +
     my_grid()
-my_ggsave(out_file_2, height=3, width=5)
+my_ggsave(out_file_2, height = 3, width = 5)
 
-# save the table 
-fwrite(tdf[tdf$p_value <= 1], out_file_3, sep="\t")
+# save the table
+fwrite(tdf[tdf$p_value <= 1], out_file_3, sep = "\t")
 # save the bed9
-# 
+#
 tdf %>%
     mutate(
-        score = pmin(round(1/p_value), 1000),
+        score = pmin(round(1 / p_value), 1000),
         name = paste0(
-            round(p_value,5), "_",
-            round(100*hap1_frac_acc,2), "_",
-            round(100*hap2_frac_acc,2)
+            round(p_value, 5), "_",
+            round(100 * hap1_frac_acc, 2), "_",
+            round(100 * hap2_frac_acc, 2)
         ),
-        tst=st+1,
-        ten=case_when(
-            p_value<=p_threshold ~ en - 1,
-            TRUE ~ st+2
+        tst = st + 1,
+        ten = case_when(
+            p_value <= p_threshold ~ en - 1,
+            TRUE ~ st + 2
         ),
-        strand=".",
+        strand = ".",
         color = case_when(
             hap1_frac_acc > hap2_frac_acc & p_value > p_threshold ~ "0,0,100",
             hap1_frac_acc > hap2_frac_acc ~ "0,0,255",
@@ -263,4 +266,4 @@ tdf %>%
         )
     ) %>%
     select(c("#chrom", "st", "en", "name", "score", "strand", "tst", "ten", "color")) %>%
-    fwrite(out_file_4, sep="\t", quote=F, na=".", row.names=F)
+    fwrite(out_file_4, sep = "\t", quote = F, na = ".", row.names = F)
